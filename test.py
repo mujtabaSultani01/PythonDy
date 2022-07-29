b5d1513f4bb2bfc19aa300ce75ccd0ba9e2c4f45
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-07-29 06:32:34 +0430

    Variables, if-else statements are coverd.

--- test.py
+++ test.py
@@ -1,1 +1,34 @@
 print("Hello World")
+
+base = 14
+height = 10
+
+area = 1/2 * (base * height)
+
+print("Rectangle area is: ", area)
+
+num = input("Pleas! Enter a number: ")
+num = int(num)
+
+if(num%2 == 0):
+    print("Your inserted number is even.")
+
+else:
+    print("Your inserted number is odd.")
+
+indian = ["Samosa","daal","naan"]
+italian = ["Pasta", "Pizza", "Pot Sticker"]
+Chiness = ["egg role", "risotto", "fried rice"]
+
+dish = input("Type your favorite dish to till you the country: ")
+
+if dish in indian:
+     print("You choosed indian dish.")
+elif dish in italian:
+     print("You choosed italian dish.")
+elif dish in Chiness:
+     print("You choosed Chiness dish.")
+else:
+     print("Based on little knowledge I have, I don't know which dish this is! ", dish)
+
+
